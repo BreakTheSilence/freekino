12e94692265ca8016ead75697dd114455d4ba47f
Kirill <kijufe@ttu.ee>
2020-05-25 00:19:24 +0300

    Search fixed

--- player/views.py
+++ player/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.core.paginator import Paginator
 from player.models import Films
+from itertools import chain
 
 
 def index(request):
@@ -10,7 +11,9 @@ def index(request):
 def films(request, page_number=1):
     search = request.GET.get("search", "")
     if search:
-        all_films = Films.objects.filter(title__icontains=search)
+        all_films_orig = Films.objects.filter(title__icontains=search)
+        all_films_cap = Films.objects.filter(title__icontains=search.title())
+        all_films = list(chain(all_films_orig, all_films_cap))
     else:
         all_films = Films.objects.all()
     current_page = Paginator(all_films, 10)
